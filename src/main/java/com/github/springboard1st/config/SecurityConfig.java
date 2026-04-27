90c60ac92c4356883ad7c6eb4062c7b481c3516c
mseojin60 <mseojin60@gmail.com>
2026-04-27 19:55:06 +0900

    Update SecurityConfig.java

--- src/main/java/com/github/springboard1st/config/SecurityConfig.java
+++ src/main/java/com/github/springboard1st/config/SecurityConfig.java
@@ -23,6 +23,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of(
+                    "http://localhost:5174"
+                ));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+                return config;
+                }))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
