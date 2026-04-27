51261bf35f60d85bcfcda1c6f655f4a04ac72f20
SEO198 <wish0725@naver.com>
2026-04-27 22:39:11 +0900

    feat: comments, likes API JWT 인증 허용 추가

--- src/main/java/com/github/springboard1st/config/SecurityConfig.java
+++ src/main/java/com/github/springboard1st/config/SecurityConfig.java
@@ -44,6 +44,8 @@ public class SecurityConfig {
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/index.html", "/").permitAll()  // 프론트 정적 파일 허용
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/posts").permitAll()
+                        .requestMatchers("/api/comments/**").permitAll()
+                        .requestMatchers("/api/likes/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
